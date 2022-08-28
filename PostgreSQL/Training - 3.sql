b50f8ed39acfd68beb820e1c0ccf74111865743a
Muhammet Dinç <gresta.bilisim@gmail.com>
2022-08-28 21:17:07 +0300

    Commit 46 | PostgreSQL Training #3 Created

---
+++ PostgreSQL/Training - 3.sql
@@ -0,0 +1,12 @@
+SELECT country FROM country
+WHERE country ~~ 'A%a';
+
+SELECT country FROM country
+WHERE country ~~ '%_____n';
+
+SELECT title FROM film
+WHERE title ~~* '%T%T%T%T%';
+
+SELECT * FROM film
+WHERE title ~~ 'C%' AND length > '90' AND rental_rate = '2.99';
+
